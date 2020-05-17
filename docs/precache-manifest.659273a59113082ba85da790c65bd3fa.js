0e5adfb44172281899687d546358a88674837f17
Sandra RO <romeroortiz.sandra@gmail.com>
2020-05-17 05:16:05 +0200

    Init

---
+++ docs/precache-manifest.659273a59113082ba85da790c65bd3fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7be558c413fa17e5386e997a8051909",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f0c4c89fc286821330f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5acb8cec1dbdcf391aa7",
+    "url": "/static/js/2.bc008b75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc008b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0c4c89fc286821330f",
+    "url": "/static/js/main.edf5802a.chunk.js"
+  },
+  {
+    "revision": "82dec2ec6f1fc1d3339b",
+    "url": "/static/js/runtime-main.6440157a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
