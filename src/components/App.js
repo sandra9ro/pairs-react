62394f384120b6dc39806ad6f1096addff0186dd
Sandra RO <romeroortiz.sandra@gmail.com>
2020-09-15 19:54:02 +0200

    change text

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Preparada para programar
+         Preparada para programar!
         </p>
       </header>
     </div>
