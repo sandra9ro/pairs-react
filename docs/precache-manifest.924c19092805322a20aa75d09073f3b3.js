08c62ce86e3c260dfc1638f5e9c7cb518e47dc50
Sandra RO <romeroortiz.sandra@gmail.com>
2020-09-15 19:54:37 +0200

    Update docs

---
+++ docs/precache-manifest.924c19092805322a20aa75d09073f3b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5c55df29393f7b5a0c0566cc8d501b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99baa95f0028bdb492df",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5acb8cec1dbdcf391aa7",
+    "url": "/static/js/2.bc008b75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc008b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99baa95f0028bdb492df",
+    "url": "/static/js/main.3d86ef6d.chunk.js"
+  },
+  {
+    "revision": "82dec2ec6f1fc1d3339b",
+    "url": "/static/js/runtime-main.6440157a.js"
+  },
+  {
+    "revision": "cd386015f3b1303e63f63e9173a999b9",
+    "url": "/static/media/foto-03.cd386015.jpg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a334d786fdbd9cb7d18104fdfc04f03e.js"
+  "/precache-manifest.924c19092805322a20aa75d09073f3b3.js"
 );
 
 self.addEventListener('message', (event) => {
