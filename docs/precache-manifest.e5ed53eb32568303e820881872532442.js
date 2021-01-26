05aaedaa723f869a2f4f93fa80bfb6425d503ca9
Sandra RO <romeroortiz.sandra@gmail.com>
2021-01-26 14:12:19 +0100

    change background color, change home page in package.json  and  and update docs

---
+++ docs/precache-manifest.e5ed53eb32568303e820881872532442.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5e6fbbd091894af5cbf31892236272",
+    "url": "./index.html"
+  },
+  {
+    "revision": "58c40828a1ee8b8d11d1",
+    "url": "./static/css/main.b6e37dba.chunk.css"
+  },
+  {
+    "revision": "1ca5a15c2e14758262e2",
+    "url": "./static/js/2.b39bdbbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b39bdbbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c40828a1ee8b8d11d1",
+    "url": "./static/js/main.783bc72c.chunk.js"
+  },
+  {
+    "revision": "357f05800af9ff538cd4",
+    "url": "./static/js/runtime-main.6d6c566a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ee671b385a758c209487b2cb10fe3a1b.js"
+  "./precache-manifest.e5ed53eb32568303e820881872532442.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
